14282e452cf367db15b57e6980f5ef553e595d0f
Ichsan Wahyudi <ichsan.wahyudi.lucky@gmail.com>
2020-12-25 18:19:20 +0700

    [UPDATE] Remove gzip command

--- pg_backup.sh
+++ pg_backup.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-OUTPUT_FILE=${DB_NAME}_$(date +"%m_%d_%Y-%H_%M_%S")_DUMP.tar
+OUTPUT_FILE=${DB_NAME}_$(date +"%m_%d_%Y-%H_%M_%S")_dump.tar
 
 PGPASSWORD=${DB_PASSWORD} pg_dump -v -U ${DB_USERNAME} -h ${DB_HOST} -p ${DB_PORT} -W -F t ${DB_NAME} > ./${OUTPUT_FILE}
 
 if [ "${?}" -eq 0 ]; then
-  gzip ${OUTPUT_FILE} \
-    && aws s3 cp ${OUTPUT_FILE}.gz s3://${S3_BUCKET} \
-    && rm ${OUTPUT_FILE}.gz
+  aws s3 cp ${OUTPUT_FILE} s3://${S3_BUCKET} \
+    && rm ${OUTPUT_FILE}
 else
   echo "Error backing up postgres"
   exit 1
